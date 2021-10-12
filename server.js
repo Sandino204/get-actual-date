7c39ebbde9576d0f993abb068c22218c30015809
Sandino204 <caiogustavoxaviermelo@gmail.com>
2021-10-12 15:36:17 -0300

    Finish

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 // where your node app starts
+const date = require("date-and-time")
 
 // init project
 var express = require('express');
@@ -24,9 +25,29 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//get Time
+app.get("/api/:time", (req, res) => {
+  const {time} = req.params
+  const isString = isNaN(time)
+  const valid = isString ? (new Date(time)).getTime() > 0 : (new Date(parseInt(time))).getTime()
+  const timeDate = isString ? new Date(time) : new Date(parseInt(time))
+
+  if(!valid){
+    return res.status(200).json({
+      success: false, 
+      message: "Unix invalid"
+    })
+  }
+
+  return res.status(200).json({
+    unix: timeDate.getTime(), 
+    utc: date.format(timeDate, "ddd, DD ddd YYYY HH:mm:ss " + "GMT")
+  })
+})
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(5000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
